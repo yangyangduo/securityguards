19c49dd3fddbbfcaa9f050358f8ff9da38abb88f
yangyangduo <zhaoyang@vip.163.com>
2013-12-27 17:12:08 +0800

    add new view

---
+++ securityguards/SensorsDisplayPanel.h
@@ -0,0 +1,13 @@
+//
+//  SensorsDisplayPanel.h
+//  securityguards
+//
+//  Created by Zhao yang on 12/27/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SensorsDisplayPanel : UIView
+
+@end
