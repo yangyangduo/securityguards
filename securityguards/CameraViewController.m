c837227963164300b314fcc5f62b40ac10052a99
yangyangduo <zhaoyang@vip.163.com>
2013-12-31 11:53:30 +0800

    fuck

---
+++ securityguards/CameraViewController.m
@@ -0,0 +1,38 @@
+//
+//  CameraViewController.m
+//  securityguards
+//
+//  Created by Zhao yang on 12/31/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
