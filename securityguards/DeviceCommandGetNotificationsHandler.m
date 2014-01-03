b8594e0f084887c9f0af0644e082d21773042ff1
yangyangduo <zhaoyang@vip.163.com>
2014-01-03 10:21:30 +0800

    ff

--- securityguards/DeviceCommandGetNotificationsHandler.m
+++ securityguards/DeviceCommandGetNotificationsHandler.m
@@ -7,31 +7,33 @@
 //
 
 #import "DeviceCommandGetNotificationsHandler.h"
-//#import "NotificationsFileManager.h"
+#import "NotificationsFileManager.h"
 #import "XXEventSubscriptionPublisher.h"
 #import "NotificationsFileUpdatedEvent.h"
-//#import "SystemAudio.h"
+#import "SystemAudio.h"
+#import "GlobalSettings.h"
+#import "CommandFactory.h"
 
 @implementation DeviceCommandGetNotificationsHandler
 
 - (void)handle:(DeviceCommand *)command {
     [super handle:command];
-//    
-//    if([command isKindOfClass:[DeviceCommandUpdateNotifications class]]) {
-//        DeviceCommandUpdateNotifications *receivedNotificationsCommand = (DeviceCommandUpdateNotifications *)command;
-//        [[NotificationsFileManager fileManager] writeToDisk:receivedNotificationsCommand.notifications];
-//        
-//        [[XXEventSubscriptionPublisher defaultPublisher] publishWithEvent:[[NotificationsFileUpdatedEvent alloc] init]];
-//        
-//        if([COMMAND_PUSH_NOTIFICATIONS isEqualToString:receivedNotificationsCommand.commandName]) {
-//            if([SMShared current].settings.isVoice) {
-//                [SystemAudio playClassicSmsSound];
-//            }
-//            if([SMShared current].settings.isShake) {
-//                [SystemAudio shake];
-//            }
-//        }
-//    }
+    
+    if([command isKindOfClass:[DeviceCommandUpdateNotifications class]]) {
+        DeviceCommandUpdateNotifications *receivedNotificationsCommand = (DeviceCommandUpdateNotifications *)command;
+        [[NotificationsFileManager fileManager] writeToDisk:receivedNotificationsCommand.notifications];
+        
+        [[XXEventSubscriptionPublisher defaultPublisher] publishWithEvent:[[NotificationsFileUpdatedEvent alloc] init]];
+
+        if([COMMAND_PUSH_NOTIFICATIONS isEqualToString:receivedNotificationsCommand.commandName]) {
+            if([GlobalSettings defaultSettings].isVoice) {
+                [SystemAudio playClassicSmsSound];
+            }
+            if([GlobalSettings defaultSettings].isShake) {
+                [SystemAudio shake];
+            }
+        }
+    }
 }
 
 @end
